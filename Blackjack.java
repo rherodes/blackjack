8af1174958a90fd3b7992f5dd1b3d86e896a092c
Rick Herodes <51223848+rherodes@users.noreply.github.com>
2020-06-30 22:38:02 -0400

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -3,7 +3,7 @@
 //	author: Rick Herodes
 //	date:	4/24/2019
 //
-//  Creates a game of blackjack.
+//  Creates GUI in which to play a game of blackjack.
 //********************************************************************
 
 import javax.swing.JFrame;
